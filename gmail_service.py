08338e35445310e4c1fdf0959a32eadb74e78e0f
Thrayambhakesh A <thrayambhak@gmail.com>
2026-05-04 20:41:14 +0530

    Fixed OAuth session state issue

--- gmail_service.py
+++ gmail_service.py
@@ -40,7 +40,7 @@ def get_user_credentials(auth_code):
         {
             "web": {
                 "client_id": st.secrets["GOOGLE_CLIENT_ID"],
-                "client_secret": st.serets["GOOGLE_CLIENT_SECRET"],
+                "client_secret": st.secrets["GOOGLE_CLIENT_SECRET"],
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                 "token_uri": "https://oauth2.googleapis.com/token",
                 "redirect_uris": [st.secrets["REDIRECT_URI"]],
