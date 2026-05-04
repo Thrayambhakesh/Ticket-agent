9a39c29ede2e6396ca9eb964dc7b0737d686b333
Thrayambhakesh A <thrayambhak@gmail.com>
2026-05-04 22:40:03 +0530

    Modifications

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from gmail_service import authenticate_user, get_user_credentials
 from gmail_service import (
     fetch_unread_emails,
-    get_user_email,
     send_email,
     archive_email,
     delete_email,
@@ -40,7 +39,7 @@ elif "code" in query_params:
         code = query_params["code"]
         creds = get_user_credentials(code)
         st.session_state.gmail_creds = creds
-        st.session_state.user_email = get_user_email(creds)
+        
         # Clear URL params after successful login
         st.query_params.clear()
 
@@ -113,7 +112,7 @@ if fetch_button:
 
                     ticket_data = {
                         "gmail_id": email["id"],
-                        "user_email": st.session_state.user_email,
+                        "user_email": st.session_state.gmail_creds.id_token["email"],
                         "sender": email["sender"],
                         "subject": email["subject"],
                         "body": email["body"][:1500],
@@ -133,7 +132,7 @@ if fetch_button:
 
 # ---------------- LOAD TICKETS ----------------
 try:
-    response = get_tickets(st.session_state.user_email)
+    response = get_tickets(st.session_state.gmail_creds.id_token["email"])
     tickets = response.data
 except Exception as e:
     st.error(f"Supabase Error: {str(e)}")
--- gmail_service.py
+++ gmail_service.py
@@ -27,11 +27,15 @@ def authenticate_user():
         redirect_uri=st.secrets["REDIRECT_URI"]
     )
 
-    auth_url, _ = flow.authorization_url(
+    auth_url, state = flow.authorization_url(
         access_type="offline",
         prompt="consent",
         include_granted_scopes="true"
     )
+
+    # Store verifier + state
+    st.session_state["oauth_state"] = state
+    st.session_state["code_verifier"] = flow.code_verifier
 
     return auth_url
 def get_user_credentials(auth_code):
@@ -46,11 +50,13 @@ def get_user_credentials(auth_code):
             }
         },
         scopes=SCOPES,
+        state=st.session_state.get("oauth_state"),
         redirect_uri=st.secrets["REDIRECT_URI"]
     )
 
     flow.fetch_token(
-        code=auth_code
+        code=auth_code,
+        code_verifier=st.session_state["code_verifier"]
     )
 
     return flow.credentials
@@ -60,14 +66,7 @@ def get_gmail_service(credentials=None):
         credentials.refresh(Request())
 
     return build("gmail", "v1", credentials=credentials)
-def get_user_email(credentials):
-    service = get_gmail_service(credentials)
 
-    profile = service.users().getProfile(
-        userId="me"
-    ).execute()
-
-    return profile["emailAddress"]
 
 # ---------------- FETCH EMAILS ----------------
 def fetch_unread_emails(credentials):
